5729819735ab31335bab1c535666509a8a066b88
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-04-28 18:05:26 -0300

    Resollucion cap1 y 2
    se hizo el uni 1 completo y el uni2 cap 1 lab01 (sin para pensar)

---
+++ Unidad02/Capitulo01/Lab01/Iteracion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] textos = new string[cantIteraciones];
+
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine("Ingrese texto " + (i+1) + ": ");
+                textos[i] = Console.ReadLine();
+            }
+
+            for (int i = cantIteraciones-1; i >= 0; i--)
+            {
+                Console.WriteLine(textos[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
