de4618a08760f34e08b0aff7bc6efb10094fc6dc
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-05-02 23:15:15 -0300

    Resolucion ej 1 y 2
    Lab01.paraPensar

---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/Biciestos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Biciestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio;
+            bool bisiesto = false;
+
+            Console.Write("Ingrese un año: ");
+            anio = int.Parse(Console.ReadLine());
+
+            if(anio % 4 == 0)
+            {
+                if (anio % 100 == 0)
+                {
+                    if(anio % 400 == 0)
+                    {
+                        bisiesto = true;
+                    }
+                }
+                else
+                {
+                    bisiesto = true; 
+                }
+            }
+
+            if (bisiesto)
+            {
+                Console.WriteLine(anio + " es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine(anio + " no es bisiesto.");
+            }b
+
+
+        }
+    }
+}
