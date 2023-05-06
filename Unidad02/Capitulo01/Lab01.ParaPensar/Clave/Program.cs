c762537b27314fb4b90f46b9b5693a0357aa831d
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-05-06 20:35:54 -0300

    Resolucion completa de Lab01 para pensar
    unidad 2 cap 1

---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/Clave/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Clave
+{
+    internal class Program
+    {
+        private const string clave = "hola123";
+        static void Main(string[] args)
+        {
+            if (ingresoClave())
+            {
+                Console.WriteLine("Clave Correcta");
+            }
+            else
+            {
+                Console.WriteLine("3 intentos fallidos, fin de programa.");
+            }
+            Console.ReadKey();
+            
+        }
+
+        public static bool ingresoClave()
+        {
+            int intentos = 0;
+
+            while (intentos < 4)
+            {
+                intentos++;
+
+                Console.WriteLine("Ingrese clave: ");
+                string input = Console.ReadLine();
+
+                if (input == clave)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/PrimosGemelos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PrimosGemelos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, antNum, max,i = 0;
+            num = antNum = 1;
+
+            Console.Write("Ingrese maximo de numeros primos gemelos a calcular: ");
+            max = int.Parse(Console.ReadLine());
+
+            while(i < max)
+            {
+                if (EsPrimo(num))
+                {
+                    if((num-antNum) == 2)
+                    {
+                        Console.WriteLine(antNum + " " + num);
+                        i++;
+                    }
+                    antNum = num;
+                    
+                }
+                num++;
+            }
+            Console.ReadKey();
+        }
+
+        public static bool EsPrimo(int num)
+        {
+            if(num < 2)
+            {
+                return false;
+            }
+            else
+            {
+                for(int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if(num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
