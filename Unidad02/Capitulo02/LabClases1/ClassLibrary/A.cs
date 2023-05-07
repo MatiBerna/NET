dec63e9c92d419e7d20b086d8d6bea78c7d65087
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-05-06 23:21:16 -0300

    Copiado de GDrive
    Se pasó lo copiado de drive a la carpeta y se intentó sin exito seguir con los ejercicios

---
+++ Unidad02/Capitulo02/LabClases1/ClassLibrary/A.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class A
+    {
+        string NombreInstancia;
+
+        public A() {
+            NombreInstancia = "Instancia sin Nombre";
+        }
+
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+
+        public string MostrarNombre()
+        {
+            return NombreInstancia;
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("El metodo M1 fue invocado");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("El metodo M2 fue invocado");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("El metodo M3 fue invocado");
+        }
+    }
+}
